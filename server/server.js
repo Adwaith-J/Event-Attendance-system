bf299c34f1bcae88246c485d949dc5cdc228c629
Adwaith J <adwaithjithesh13@gmail.com>
2025-09-12 21:55:09 +0530

    Fix catch-all route for React frontend

--- server/server.js
+++ server/server.js
@@ -32,9 +32,10 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
+
 
 // Start server
 const PORT = process.env.PORT || 4000;
